1425f35bdfd2d139728940dd299fa29f4389547c
kiwi <kiwi71728@icloud.com>
2021-06-06 16:21:26 +0800

    refactor: support bash 3

--- src/completion.sh
+++ src/completion.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+YC_COMPLETION_SCRIPT_LOCATION=$(dirname "${BASH_SOURCE[0]}")
+
 YC_COMMAND_WORDS_VARNAME_PREFIX="YARN_COMMAND_WORDS_"
 YC_COMMAND_END_MARK="yarn_command_end_mark_for_prorcesing_last_word"
 YC_ALTRENATIVE_FLAG_SYMBOL="|"
@@ -17,6 +19,9 @@ declare -a YARN_COMMAND_WORDS_REFS=()
 YC_WORKSPACE_PACKAGES=
 YC_TMP_EXPANDED_VAR_RESULT=
 
+IS_SUPPORT_DECLARE_N_FLAG=1
+IS_SUPPORT_NEGATIVE_NUMBER_SUBSCRIPT=1
+
 get_yarn_workspace_packages() {
   local package_json_path
   local node_commands
@@ -28,7 +33,7 @@ get_yarn_workspace_packages() {
     return 0
   fi
 
-  packages_path=($(node -e "console.log(require('${YC_ROOT_PACKAGE_PATH}').workspaces.join(' '))"))
+  packages_path=($(node -e "console.log((require('${YC_ROOT_PACKAGE_PATH}').workspaces || []).join(' '))"))
 
   for package_path in "${packages_path[@]}"; do
     package_json_path="./${package_path}/package.json"
@@ -42,7 +47,7 @@ get_yarn_workspace_packages() {
 
   package_names=($YC_WORKSPACE_PACKAGES)
 
-  for index in $(seq "-${#package_names[@]}" -1); do
+  for (( index=0; index<${#package_names[@]}; ++index )); do
     set_package_name_path_map "${package_names[$index]}" "${packages_path[$index]}"
   done
 }
@@ -52,17 +57,7 @@ expand_workspaceName_variable() {
 }
 
 expand_commandName_variable() {
-  local package_name="$1"
-
-  local package_path_var_name
-
-  package_path_var_name=$(get_var_name_for_package_name "${package_name}")
-
-  declare -n package_path_ref="${package_path_var_name}"
-
-  if [[ -f "${package_path_ref}" ]]; then
-    :
-  fi
+  :
 }
 
 yc_get_expand_var() {
@@ -88,16 +83,33 @@ yc_get_expand_var() {
 }
 
 expand_yarn_workspace_command_list() {
-  declare -n yarn_command_workspace_ref="$1"
+  if [[ $IS_SUPPORT_DECLARE_N_FLAG -eq 1 ]]; then
+    declare -n yarn_command_workspace_ref="$1"
+    declare -n yarn_command_words_ref
+  else
+    local yarn_command_workspace_ref="$1"
+    local yarn_command_words_ref
+  fi
+
   declare -i store_yarn_command_index=$2
-  declare -n yarn_command_words_ref
   
   local word
   local workspace_recursive_command
   local store_yarn_command_var_name
   local appending_words
 
+  if [[ $IS_SUPPORT_DECLARE_N_FLAG -eq 0 ]]; then
+    yarn_command_workspace_ref+="[@]"
+    yarn_command_workspace_ref=("${!yarn_command_workspace_ref}")
+  fi
+
   for yarn_command_words_ref in "${YARN_COMMAND_WORDS_REFS[@]}"; do
+    if [[ $IS_SUPPORT_DECLARE_N_FLAG -eq 0 ]]; then
+      yarn_command_words_ref+="[@]"
+      yarn_command_words_ref=("${!yarn_command_words_ref}")
+    fi
+
+
     if [[ ${yarn_command_words_ref[*]} = "yarn workspace"* ]] || \
       [[ ${yarn_command_words_ref[*]} = "yarn workspaces"* ]]; then
       continue
@@ -117,10 +129,14 @@ expand_yarn_workspace_command_list() {
     store_yarn_command_var_name="${YC_COMMAND_WORDS_VARNAME_PREFIX}${store_yarn_command_index}"
     store_yarn_command_index+=1
 
-    declare -n store_yarn_command_ref="${store_yarn_command_var_name}"
-
     YARN_COMMAND_WORDS_REFS[${#YARN_COMMAND_WORDS_REFS[@]}]="${store_yarn_command_var_name}"
-    store_yarn_command_ref=("${workspace_recursive_command[@]}")
+    if [[ $IS_SUPPORT_DECLARE_N_FLAG -eq 1 ]]; then
+      declare -n store_yarn_command_ref="${store_yarn_command_var_name}"
+      store_yarn_command_ref=("${workspace_recursive_command[@]}")
+    else
+      local store_yarn_command_ref="${store_yarn_command_var_name}"
+      eval "$store_yarn_command_var_name=("'"''${workspace_recursive_command[@]}''"'")"
+    fi
   done
 }
 
@@ -137,6 +153,7 @@ generate_yarn_command_list() {
   local store_yarn_command_var_name
   local broken_word=""
   local yarn_command_workspace_ref
+  local subscript
 
   local flags=()
 
@@ -175,7 +192,8 @@ generate_yarn_command_list() {
           assembling_flag="${assembling_flag%>}"
 
           if [[ $previous_word_is_flag -eq 1 ]]; then
-            yarn_command_words[-1]+="${YC_FLAG_GROUP_CONCAT_SYMBOL}${assembling_flag}"
+            [[ $IS_SUPPORT_NEGATIVE_NUMBER_SUBSCRIPT -eq 1 ]] && subscript=-1 || { subscript=${#yarn_command_words[@]}; : $(( subscript-- )); }
+            yarn_command_words[subscript]+="${YC_FLAG_GROUP_CONCAT_SYMBOL}${assembling_flag}"
       
           else
             yarn_command_words+=("${assembling_flag}")
@@ -186,12 +204,14 @@ generate_yarn_command_list() {
         fi
       else
         if [[ $previous_word_is_flag -eq 1 ]]; then
+          [[ $IS_SUPPORT_NEGATIVE_NUMBER_SUBSCRIPT -eq 1 ]] && subscript=-1 || { subscript=${#yarn_command_words[@]}; : $(( subscript-- )); }
           IFS=','
-          flags=("${yarn_command_words[-1]}")
+          flags=(${yarn_command_words[subscript]})
           IFS="$ORI_IFS"
 
           for (( i=0; i<${#flags[@]}; ++i )); do
-            yarn_command_words+=("${yarn_command_words[-1]}")
+            [[ $IS_SUPPORT_NEGATIVE_NUMBER_SUBSCRIPT -eq 1 ]] && subscript=-1 || { subscript=${#yarn_command_words[@]}; : $(( subscript-- )); }
+            yarn_command_words+=("${yarn_command_words[subscript]}")
           done
         fi
 
@@ -208,10 +228,14 @@ generate_yarn_command_list() {
     store_yarn_command_var_name="${YC_COMMAND_WORDS_VARNAME_PREFIX}${store_yarn_command_index}"
     store_yarn_command_index+=1
 
-    declare -n store_yarn_command_ref="${store_yarn_command_var_name}"
-
     YARN_COMMAND_WORDS_REFS[${#YARN_COMMAND_WORDS_REFS[@]}]="${store_yarn_command_var_name}"
-    store_yarn_command_ref=("${yarn_command_words[@]}")
+
+    if [[ IS_SUPPORT_DECLARE_N_FLAG -eq 1 ]]; then
+      declare -n store_yarn_command_ref="${store_yarn_command_var_name}"
+      store_yarn_command_ref=("${yarn_command_words[@]}")
+    else
+      eval "$store_yarn_command_var_name=("'"''${yarn_command_words[@]}''"'")"
+    fi
 
     if [[ $instruction = *"yarn workspace "* ]]; then
       yarn_command_workspace_ref="${store_yarn_command_var_name}"
@@ -319,7 +343,11 @@ yc_add_word_candidates() {
 }
 
 yc_run_yarn_completion() {
-  declare -n yarn_command_words
+  if [[ $IS_SUPPORT_DECLARE_N_FLAG -eq 1 ]]; then
+    declare -n yarn_command_words
+  else
+    local yarn_command_words
+  fi
 
   local completing_word="$1"
   local word_num="${#COMP_WORDS[@]}"
@@ -334,6 +362,11 @@ yc_run_yarn_completion() {
   COMPREPLY=()
 
   for yarn_command_words in "${YARN_COMMAND_WORDS_REFS[@]}"; do
+    if [[ $IS_SUPPORT_DECLARE_N_FLAG -eq 0 ]]; then
+      yarn_command_words+="[@]"
+      yarn_command_words=("${!yarn_command_words}")
+    fi
+
 
     for (( comp_word_index=0; comp_word_index<$last_word_index; ++comp_word_index )); do
       yc_get_identified_word "${yarn_command_words[$comp_word_index]}"
@@ -393,11 +426,16 @@ set_package_name_path_map() {
   local var_name_for_package_name
 
   var_name_for_package_name=$(get_var_name_for_package_name "$package_name")
-  declare -n package_name_path_ref="$var_name_for_package_name"
-  package_name_path_ref="$package_path"
+  if [[ IS_SUPPORT_DECLARE_N_FLAG -eq 1 ]]; then
+    declare -n package_name_path_ref="$var_name_for_package_name"
+    package_name_path_ref="$package_path"
+  else
+    eval "$var_name_for_package_name="'"'"${package_path}"'"'
+  fi
 }
 
-dcard_yarn_completion_main() {
+yc_yarn_completion_main() {
+
   if [ -z "$BASH_VERSINFO" ]; then
     echo "Sorry, the yarn completion only supports BASH" 1>&2
     return 1
@@ -408,10 +446,18 @@ dcard_yarn_completion_main() {
     return 1
   fi
 
+  if ! declare -n declare_n_flag_test > /dev/null 2>&1; then
+    IS_SUPPORT_DECLARE_N_FLAG=0
+  fi
+
+  if ! "${YC_COMPLETION_SCRIPT_LOCATION}/syntax-checker/negative-subscript.sh" > /dev/null 2>&1; then
+    IS_SUPPORT_NEGATIVE_NUMBER_SUBSCRIPT=0
+  fi
+
   generate_yarn_command_list
   get_yarn_workspace_packages
 
-  complete -D -F yc_yarn_completion_for_complete -o bashdefault -o default yarn
+  complete -F yc_yarn_completion_for_complete -o bashdefault -o default yarn
 }
 
-dcard_yarn_completion_main
+yc_yarn_completion_main
---
+++ src/syntax-checker/negative-subscript.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+main() {
+  set -e
+  local a=(100)
+  a[-1]=0
+}
+
+main
