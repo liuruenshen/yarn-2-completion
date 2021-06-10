31b6c5875f2a30030dee67bccc74bde60a6a8b91
kiwi <kiwi71728@icloud.com>
2021-06-10 16:23:49 +0800

    refactor: reuse iterating docker files logic

--- src/dev/build-docker.sh
+++ src/dev/build-docker.sh
@@ -3,24 +3,18 @@
 # shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/../utilities/docker.sh"
 
+build_docker_callback() {
+  local docker_file="$1"
+  local docker_tag="$2"
+  local docker_image_id="$3"
+
+  if [[ -z $docker_image_id ]]; then
+    build_docker "${docker_file}" "${docker_tag}"
+  fi
+}
+
 main() {
-  local script_dir=""
-
-  script_dir=$(dirname "${BASH_SOURCE[0]}")
-
-  local docker_files=("${script_dir}"/../../docker/*)
-  local docker_file=""
-  local docker_tag_name=""
-  local docker_image_id=""
-
-  for docker_file in "${docker_files[@]}"; do
-    docker_tag_name=$(get_docker_tag "${docker_file}")
-    docker_image_id=$(get_docker_image_id "${docker_tag_name}")
-
-    if [[ -z $docker_image_id ]]; then
-      build_docker "${docker_file}" "${docker_tag_name}"
-    fi
-  done
+  docker_iterate "build_docker_callback"
 }
 
 main
--- src/utilities/docker.sh
+++ src/utilities/docker.sh
@@ -25,3 +25,28 @@ get_docker_tag() {
   docker_file_name=$(basename "${docker_file}")
   echo "yarn-2-completion-test:${docker_file_name%%.*}"
 }
+
+docker_iterate() {
+  local invoke_func="$1"
+  local script_dir=""
+
+  if ! declare -f "${invoke_func}" >/dev/null 2>&1; then
+    return 1
+  fi
+
+  script_dir=$(dirname "${BASH_SOURCE[0]}")
+
+  local docker_files=("${script_dir}"/../../docker/*)
+  local docker_file=""
+  local docker_tag_name=""
+  local docker_image_id=""
+
+  for docker_file in "${docker_files[@]}"; do
+    docker_tag_name=$(get_docker_tag "${docker_file}")
+    docker_image_id=$(get_docker_image_id "${docker_tag_name}")
+
+    $invoke_func "${docker_file}" "${docker_tag_name}" "${docker_image_id}"
+  done
+
+  return 0
+}
