9308869c27fbe936eeae70225553099a3d058614
kiwi <kiwi71728@icloud.com>
2021-07-05 08:28:03 +0800

    test: substitute shellspec for bats-core

--- src/dev/run-test.sh
+++ src/dev/run-test.sh
@@ -17,10 +17,10 @@ run_test_in_docker() {
 
   echo "Running test on ${docker_tag}"
   # shellcheck disable=SC2140
-  docker run -t --rm -v "${PWD}/${residing_path}/../":"/yarn-2-completion/src" \
-    -v "${PWD}/${residing_path}/../../test":"/yarn-2-completion/test" \
-    -v "${PWD}/${residing_path}/../../":"/yarn-2-completion-integ-test" \
-    "${docker_tag}" /yarn-2-completion/test/bats/bin/bats "$@" /yarn-2-completion/test
+  docker run -t --rm -v "${PWD}/${residing_path}/../../":"/yarn-2-completion" \
+    -v "${PWD}/${residing_path}/../../test/yarn-repo":"/yarn-repo" \
+    "${docker_tag}" shellspec --chdir /yarn-2-completion --require yarn_command_mock \
+    --shell bash --execdir @project "$@"
 }
 
 main() {
